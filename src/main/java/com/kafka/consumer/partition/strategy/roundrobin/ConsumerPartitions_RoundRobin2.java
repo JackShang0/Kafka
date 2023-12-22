4f957c705fe75b79b7062b973ed0eacb250e4e84
shangqj <shangqj@si-tech.com.cn>
2023-12-22 20:12:15 +0800

    kafka消费者-分区消费策略roundRobin

---
+++ src/main/java/com/kafka/consumer/partition/strategy/roundrobin/ConsumerPartitions_RoundRobin2.java
@@ -0,0 +1,53 @@
+package com.kafka.consumer.partition.strategy.roundrobin;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ * @description: 消费者分区策略： roundRobin
+ * @author: shangqj
+ * @date: 2023/12/21
+ * @version: 1.0
+ */
+public class ConsumerPartitions_RoundRobin2 {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        //连接
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"119.23.70.8:9092");
+        //反序列化
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        //配置消费者 groupid
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,"roundRobin");
+
+        //指定分区策略
+        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG,"org.apache.kafka.clients.consumer.RoundRobinAssignor");
+
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String,String>(properties);
+
+        //消费的主题
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("test");
+        kafkaConsumer.subscribe(topics);
+
+
+        while (true){
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ZERO);
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord);
+            }
+        }
+
+    }
+}
